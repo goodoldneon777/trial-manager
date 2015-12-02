082d0c3713a70a8367ded50f65211993f2f10f27
Aaron Harper <aaron@Aarons-MacBook.local>
2015-12-02 17:39:24 -0500

    Improved narrow-width (xs) layout for upcoming/ongoing/recent trials and search results modules.

--- php/dist/m-ongoing-trials.php
+++ php/dist/m-ongoing-trials.php
@@ -26,32 +26,34 @@
 	$html = "";
 
 	if ($result->num_rows > 0) {
-		$html = "
-			<table class=\"table table-striped table-bordered\">\n
-				<thead style=\"text-align:center;\">\n
-					<th style=\"width:50%; text-align:center;\">Trial Name</th>\n
-					<th style=\"width:80px; text-align:center;\">Unit</th>\n
-					<th style=\"text-align:center;\">Start Date</th>\n
-					<th style=\"text-align:center;\">End Date</th>\n
-					<th style=\"text-align:center;\">Actions</th>\n
-				</thead>\n
-			";
+		$html = 
+			"<table class=\"table table-striped table-bordered\"> \n" .
+			"  <thead style=\"text-align:center;\"> \n" .
+			"    <th style=\"width:50%; text-align:center;\">Trial Name</th> \n" .
+			"    <th style=\"width:80px; text-align:center;\">Unit</th> \n" .
+			"    <th style=\"text-align:center;\">Start Date</th> \n" .
+			"    <th style=\"text-align:center;\">End Date</th> \n" .
+			"    <th class=\"hidden-xs\" style=\"text-align:center;\">Actions</th> \n" .
+			"  </thead> \n" .
+			"  <tbody> \n";
     // output data of each row
     while($row = $result->fetch_assoc()) {
-      // echo "id: " . $row["trial_name"]. " - Name: " . $row["start_dt"]. " " . $row["end_dt"]. "<br>";
-      $html .= "
-      	<tr>\n
-      		<td><a href=\"view.php?trialseq=" . $row["trial_seq"] . "\">" . $row["trial_name"] . "</a></td>\n
-      		<td>" . $row["unit"] . "</td>\n
-      		<td>" . date_format(date_create($row["start_dt"]), "m/d/Y") . "</td>\n
-      		<td>" . date_format(date_create($row["end_dt"]), "m/d/Y") . "</td>\n
-      		<td style=\"text-align:center;\">
-      			<a href=\"view.php?trialseq=" . $row["trial_seq"] . "\">View</a>
-      			|
-      			<a href=\"comment.php?trialseq=" . $row["trial_seq"] . "\">Comment</a></td>\n
-    		</tr>\n";
+      $html .= 
+      	"<tr> \n" .
+      	"  <td><a href=\"view.php?trialseq=" . $row["trial_seq"] . "\">" . $row["trial_name"] . "</a></td> \n" .
+      	"  <td>" . $row["unit"] . "</td> \n" .
+      	"  <td>" . date_format(date_create($row["start_dt"]), "m/d/Y") . "</td> \n" .
+      	"  <td>" . date_format(date_create($row["end_dt"]), "m/d/Y") . "</td> \n" .
+      	"  <td class=\"hidden-xs\" style=\"text-align:center;\"> \n" .
+      	"    <a href=\"view.php?trialseq=" . $row["trial_seq"] . "\">View</a> \n" .
+      	"    | \n" .
+      	"    <a href=\"comment.php?trialseq=" . $row["trial_seq"] . "\">Comment</a> \n" .
+      	"  </td> \n" .
+    		"</tr> \n";
     }
-    $html .= "</table>";
+    $html .=
+    	"  </tbody> \n" .
+    	"</table> \n";
 	} else {
     $html = "<div style=\"text-align:center; padding:10px;\">No trials found</div>";
 	}
--- php/src/sql/sql-search.php
+++ php/src/sql/sql-search.php
@@ -76,31 +76,32 @@
 	if ($result->num_rows > 0) {
 		$html = 
 			"<table class=\"table table-striped table-bordered\"> \n" .
-			"  <thead style=\"text-align:center;\">\n" .
+			"  <thead style=\"text-align:center;\"> \n" .
 			"    <th style=\"width:50%; text-align:center;\">Trial Name</th> \n" .
 			"    <th style=\"width:80px; text-align:center;\">Unit</th> \n" .
 			"    <th style=\"text-align:center;\">Start Date</th> \n" .
 			"    <th style=\"text-align:center;\">End Date</th> \n" .
-			"    <th style=\"text-align:center;\">Actions</th> \n" .
-			"  </thead> ";
+			"    <th class=\"hidden-xs\" style=\"text-align:center;\">Actions</th> \n" .
+			"  </thead> \n" .
+			"  <tbody> \n";
     // output data of each row
     while($row = $result->fetch_assoc()) {
       $html .= 
-      	"<tbody> \n" .
-      	"  <tr> \n" .
-      	"    <td><a href=\"view.php?trialseq=" . $row["trial_seq"] . "\">" . $row["trial_name"] . "</a></td> \n" .
-      	"    <td>" . $row["unit"] . "</td> \n" .
-      	"    <td>" . date_format(date_create($row["start_dt"]), "n/j/Y") . "</td> \n" .
-      	"    <td>" . date_format(date_create($row["end_dt"]), "n/j/Y") . "</td> \n" .
-      	"    <td style=\"text-align:center;\"> \n" .
-      	"      <a href=\"view.php?trialseq=" . $row["trial_seq"] . "\">View</a> \n" .
-      	"      | \n" .
-      	"      <a href=\"comment.php?trialseq=" . $row["trial_seq"] . "\">Comment</a> \n" .
-      	"    </td> \n" .
-    		"  </tr> \n" .
-    		"</tbody>";
+      	"<tr> \n" .
+      	"  <td><a href=\"view.php?trialseq=" . $row["trial_seq"] . "\">" . $row["trial_name"] . "</a></td> \n" .
+      	"  <td>" . $row["unit"] . "</td> \n" .
+      	"  <td>" . date_format(date_create($row["start_dt"]), "m/d/Y") . "</td> \n" .
+      	"  <td>" . date_format(date_create($row["end_dt"]), "m/d/Y") . "</td> \n" .
+      	"  <td class=\"hidden-xs\" style=\"text-align:center;\"> \n" .
+      	"    <a href=\"view.php?trialseq=" . $row["trial_seq"] . "\">View</a> \n" .
+      	"    | \n" .
+      	"    <a href=\"comment.php?trialseq=" . $row["trial_seq"] . "\">Comment</a> \n" .
+      	"  </td> \n" .
+    		"</tr> \n";
     }
-    $html .= "</table>";
+    $html .=
+    	"  </tbody> \n" .
+    	"</table> \n";
 	} else {
     $html = "<div style=\"text-align:center; padding:10px;\">No trials found</div>";
 	}
