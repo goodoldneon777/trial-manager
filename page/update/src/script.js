d26e2d35f55668ca2e681394ea9eac9665c94604
goodoldneon777 <aaron@Aarons-MacBook.local>
2015-12-20 17:51:22 -0500

    Fixed: update page wasn't redirecting after submitted

--- page/update/src/script.js
+++ page/update/src/script.js
@@ -126,8 +126,7 @@ p_update.updateFunc = function(pageType, seq) {
 						label: 'OK',
 						action: function(){
 							dialog.close();
-							// document.location.href = urlRedirect + results.seq;
-							console.log(results.debugSQL);
+							document.location.href = urlRedirect + results.seq;
 						}
 					}]
 				});
