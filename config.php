85344e17a1245978f52156c5fab7cb7e027729f1
goodoldneon777 <aaron@Aarons-MacBook.local>
2015-12-21 19:03:22 -0500

    Added commented lines for e3lapp server.

--- config.php
+++ config.php
@@ -3,7 +3,12 @@
 
 	define("SERVER_ROOT", dirname(__FILE__));
 
+
+	//For MAMP:
 	define("WEB_ROOT", "/" . basename(__DIR__));
+
+	//For e3lapp:
+	//define("WEB_ROOT", "/glw/" . basename(__DIR__));
 
 
 ?>
